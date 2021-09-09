ad0a65076506cc63becf14c885209d6262e16b77
Carlos Morais <carlos.morais@key4events.com>
2021-09-09 20:51:28 +0200

    Update

--- AmikojAPI/AmikojAPI/Startup.cs
+++ AmikojAPI/AmikojAPI/Startup.cs
@@ -10,9 +10,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AmikojApi.Models;
 using AutoMapper;
 
